bcb009f5321810be7a9994cbb3ca38893b3bf0f7
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-03-28 13:39:15 +0530

    created all components and routes

---
+++ src/app/shared/services/auth-guard/auth-guard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-guard',
+  imports: [],
+  templateUrl: './auth-guard.component.html',
+  styleUrl: './auth-guard.component.css'
+})
+export class AuthGuardComponent {
+
+}
---
+++ src/app/shared/services/login-signup/login-signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-signup',
+  imports: [],
+  templateUrl: './login-signup.component.html',
+  styleUrl: './login-signup.component.css'
+})
+export class LoginSignupComponent {
+
+}
